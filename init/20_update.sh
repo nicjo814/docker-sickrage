54485e5a4feb49ba2962c74be7bb63cd7b502ec8
Niclas Berglind <nb@kjam.se>
2015-09-08 12:19:55 +0200

    Initial push

---
+++ init/20_update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p /app
+chown abc:abc /app
+
+if [ ! -d /app/sickrage/.git ]; then 
+	/sbin/setuser abc git clone https://github.com/SiCKRAGETV/SickRage.git /app/sickrage
+else
+	cd /app/sickrage
+	/sbin/setuser abc git pull
+fi
